3bd8be3f22712b06edc67b3286472b4c1d21d38e
jezdufourq <jezdufourq@gmail.com>
2018-08-19 13:01:03 +1000

    Added button add functionality.

--- frontend_script.py
+++ frontend_script.py
@@ -5,7 +5,9 @@ import tkinter.ttk as ttk
 from tkinter import messagebox
 from calculationHandler import entryCalculation
 from db.DAO import Datastore
+from db.instruments import Instrument, Asset
 
+# Creating the window
 root=Tk()
 root.wm_title("Entry Ticket")
 calculator= entryCalculation()
@@ -13,10 +15,6 @@ calculator= entryCalculation()
 # asset frame
 asset_frame=LabelFrame(root,text="Instruments:")
 asset_frame.grid(row=0,column=0,columnspan=4)
-
-def get_instrument():
-    selected_row=tree.focus()
-    print(tree.item(selected_row)['values'][0])
 
 tree= ttk.Treeview(asset_frame)
 tree["columns"]=(1,2,3,4)
@@ -30,8 +28,7 @@ for heading in headings:
     tree.heading(column_index,text=heading)
     column_index+=1
 
-
-
+# Inserting the data into the table
 tree.insert("", "end", values=("Blah blah1",100, 3500,32))
 tree.insert("", "end", values=("Blah blah2",101, 3500,35))
 tree.insert("", "end", values=("Blah blah3",102, 3500,20))
@@ -39,12 +36,31 @@ tree.insert("", "end", values=("Blah blah4",103, 3500,31))
 tree.insert("", "end", values=("Blah blah5",104, 3500,37))
 tree.insert("", "end", values=("Blah blah6",105, 3500,39))
 tree.insert("", "end", values=("Blah blah7",106, 3500,30))
-
 tree.grid(row=0,column=0,columnspan=3)
 
-# TODO list: grabbing the value from the dictionary and displaying it in console log
-# TODO list: populate the treeviw with a list of dictionaries sample
-# TODO list: take the value of the table selected and display it
+# Input variables
+instrumentCode = StringVar()
+entryDate = StringVar()
+entryPrice = StringVar()
+targetPrice = StringVar()
+stopLoss = StringVar()
+qty = StringVar()
+technicalInput = StringVar()
+fundamentalInput = StringVar()
+commentInput = StringVar()
+marginRate = StringVar()
+exitDate = StringVar()
+exitPrice = StringVar()
+type = StringVar()
+
+# Output variables
+margin = StringVar()
+Rrr = StringVar()
+marketVal = StringVar()
+targetVal = StringVar()
+tpercentVal = StringVar()
+stoplossVal = StringVar()
+percentVal = StringVar()
 
 # entry frame
 entry_frame=LabelFrame(root,text="Please enter the values:")
@@ -53,29 +69,24 @@ entry_frame.grid(row=0,column=4,rowspan=9,columnspan=2,ipady=20,padx=10)
 # entry label
 entryLabel= Label(entry_frame,text="Entry Price:")
 entryLabel.grid(row=1,column=4,padx=10,pady=10)
-
-entryPrice=StringVar()
 entryLabel_input=Entry(entry_frame,textvariable=entryPrice)
 entryLabel_input.grid(row=1,column=5,padx=10,pady=10)
 
 # target price
 targetPriceLabel=Label(entry_frame,text="Target Price:")
 targetPriceLabel.grid(row=2,column=4,padx=10,pady=10)
-targetPrice=StringVar()
 targetLabel_input=Entry(entry_frame,textvariable=targetPrice)
 targetLabel_input.grid(row=2,column=5,padx=10,pady=10)
 
 # stop loss
 stopLossLabel=Label(entry_frame,text="S/L:")
 stopLossLabel.grid(row=3,column=4,padx=10,pady=10)
-stopLoss=StringVar()
 stopLoss_input=Entry(entry_frame,textvariable=stopLoss)
 stopLoss_input.grid(row=3,column=5,padx=10,pady=10)
 
 # Qty
 qtyLabel=Label(entry_frame,text="Qty:")
 qtyLabel.grid(row=4,column=4,padx=10,pady=10)
-qty=StringVar()
 qty_input=Entry(entry_frame,textvariable=qty)
 qty_input.grid(row=4,column=5,padx=10,pady=10)
 
@@ -105,7 +116,6 @@ calculation_frame.grid(row=0,column=7,rowspan=5,columnspan=2,ipady=10,padx=5)
 # margin rate entry
 marginRateLabel=Label(calculation_frame,text="Margin rate:")
 marginRateLabel.grid(row=0,column=7,padx=10,pady=10)
-marginRate=StringVar()
 marginRate_input=Entry(calculation_frame,textvariable=marginRate)
 marginRate_input.grid(row=0,column=8,padx=10,pady=10)
 marginRate_input.configure(state="disable")
@@ -113,7 +123,6 @@ marginRate_input.configure(state="disable")
 # margin entry
 marginLabel= Label(calculation_frame,text="Margin:")
 marginLabel.grid(row=1,column=7,padx=10,pady=10)
-margin=StringVar()
 margin_input=Entry(calculation_frame,textvariable=margin)
 margin_input.grid(row=1,column=8,padx=10,pady=10)
 margin_input.configure(state="disable")
@@ -121,16 +130,13 @@ margin_input.configure(state="disable")
 #R.R.R risk reward ration
 RrrLabel=Label(calculation_frame,text="R.R Ratio:")
 RrrLabel.grid(row=2,column=7,padx=10,pady=10)
-Rrr=StringVar()
 Rrr_input=Entry(calculation_frame,textvariable=Rrr)
 Rrr_input.grid(row=2,column=8,padx=10,pady=10)
 Rrr_input.configure(state="disable")
 
-
 # Market value
 marketValLabel=Label(calculation_frame,text="Market Value:")
 marketValLabel.grid(row=3,column=7,padx=10,pady=10)
-marketVal=StringVar()
 marketVal_input=Entry(calculation_frame,textvariable=marketVal)
 marketVal_input.grid(row=3,column=8,padx=10,pady=10)
 marketVal_input.configure(state="disable")
@@ -138,14 +144,11 @@ marketVal_input.configure(state="disable")
 # target Value
 targetValLabel=Label(calculation_frame,text="targetVal:")
 targetValLabel.grid(row=4,column=7,padx=10,pady=10)
-targetVal=StringVar()
 targetVal_input=Entry(calculation_frame,textvariable=targetVal)
 targetVal_input.grid(row=4,column=8,padx=5,pady=5)
 
 percentLabel=Label(calculation_frame,text="%")
 percentLabel.grid(row=4,column=9)
-
-tpercentVal=StringVar()
 tpercentVal_input=Entry(calculation_frame,textvariable=tpercentVal)
 tpercentVal_input.grid(row=4,column=10,ipadx=5,padx=5)
 tpercentVal_input.configure(state="disable")
@@ -154,8 +157,6 @@ targetVal_input.configure(state="disable")
 # stop loss value
 stoplossValLabel=Label(calculation_frame,text="Stop Loss Value:")
 stoplossValLabel.grid(row=5,column=7,padx=10,pady=10)
-stoplossVal=StringVar()
-percentVal=StringVar()
 stoplossVal_input=Entry(calculation_frame,textvariable=stoplossVal)
 stoplossVal_input.grid(row=5,column=8,padx=5,pady=5)
 
@@ -167,16 +168,6 @@ percentVal_input.grid(row=5,column=10,ipadx=5,padx=5)
 
 stoplossVal_input.configure(state="disable")
 percentVal_input.configure(state="disable")
-
-# Testing and manipulating inputs
-# a=entryPrice.get()
-# b=targetPrice.get()
-# print(dummy.sum(a,b))
-
-# creating new windows
-# def new_window():
-#     new_win= Toplevel(root)
-#     dashboard.create_dashboard(new_win)
 
 # enabling frame
 def enable_calcwidget():
@@ -226,9 +217,48 @@ def enable_calcwidget():
 def destroy_window():
     root.destroy()
 
+def get_instrument():
+    selected_row=tree.focus()
+    print(tree.item(selected_row)['values'][0])
+
+def add_button():
+    # Defining variables to store the data
+    instrumentVar = get_instrument()
+    entryDate = time.time()
+    entryPriceVar = entryLabel_input.get()
+    targetPriceVar = targetLabel_input.get()
+    stopLossVar = stopLoss_input.get()
+    qtyVar = qty_input.get()
+    technicalInputVar = technical_input.get()
+    fundamentalInputVar = fundamental_input.get()
+    commentInputVar = comment_input.get()
+    marginRateVar = marginRate_input.get()
+
+    # Adding to the database
+    newAsset = Asset({"instrumentCode":instrumentVar,
+                      "entryDate":entryDate,
+                      "entryPrice":entryPriceVar,
+                      "targetPrice":targetPriceVar,
+                      "stopLossPrice":stopLossVar,
+                      "qty":qtyVar,
+                      "technicals":technicalInputVar,
+                      "fundamentals":fundamentalInputVar,
+                      "commons":commentInputVar,
+                      "marginRate":marginRateVar
+                      })
+
+    # Deleting from the window
+    entryLabel_input.delete(0, END)
+    targetLabel_input.delete(0, END)
+    stopLoss_input.delete(0, END)
+    qty_input.delete(0, END)
+    technical_input.delete(0, END)
+    fundamental_input.delete(0, END)
+    comment_input.delete(0, END)
+    marginRate_input.delete(0, END)
 
 # buttons
-add_button= Button(root,text="Add")
+add_button= Button(root,text="Add", command = add_button)
 add_button.grid(row=5,column=6,ipadx=75,padx=5,pady=5,columnspan=2)
 add_button.configure(state="disable")
 
